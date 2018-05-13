37650fcc8677d46d861cd9843da5fd579ec3b001
Saravanan <saravanan.2.n@nokia.com>
2018-05-13 14:00:01 +0530

    Creating layout component

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   
   render () {
     return (
       <div>
-        <h1>Hi, I'm a React App</h1>
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
---
+++ burger-app/src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Aux from "../../hoc/Auxillary";
+
+const layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, BackDrop</div>
+    <main>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
