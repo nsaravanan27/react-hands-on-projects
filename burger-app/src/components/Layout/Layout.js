87333519d02dd951085814bcd6ba98a9931aa1e5
Saravanan <saravanan.2.n@nokia.com>
2018-05-17 16:06:13 +0530

    Sidedrawer, adjusting the response and reusing the backdrop.

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Auxillary";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <Aux>
-    <Toolbar/>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    isBackdropShown: true
+  }
 
-export default layout;
+  hideBackdropHandler = () => {
+    this.setState({
+      isBackdropShown: false
+    });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer show={this.state.isBackdropShown} clicked={this.hideBackdropHandler}/>
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
